ea4b83b4244c5c58f7c038605ebef45ed7bcfa36
ceb <vivu1991@mail.ru>
2022-09-19 18:18:44 +0300

    Решение задачи 66

---
+++ DZ002/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Summa(int m, int n)
+{
+    if (m == n) return m;
+    if (m > n) return m + Summa(n, m - 1);
+    return n + Summa(m, n - 1);
+}
+Console.WriteLine(Summa(m, n));
